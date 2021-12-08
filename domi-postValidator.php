940998e11ecddcc8b89b66644f5909161f01795d
Dominique Müller <dominique_mueller@gmx.ch>
2021-12-08 20:43:29 +0100

    initial post

---
+++ domi-postValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+* Plugin Name: Post Validator
+* Plugin URI: 
+* Description: : Validierung von Inseraten mittels wp cron
+* Version: 1.0
+* Author: Dominique Müller
+* Author URI: 
+**/
+
+
+
+
+
+
+
+$plugin_url = WP_PLUGIN_DIR . '/' . basename(dirname(__FILE__));
+
+/* include Scripts*/
+function postValidator_customcss() {
+    wp_enqueue_style( 'postValidator',  plugin_dir_url( __FILE__ ) . '/css/postValidator.css' );                      
+}
+add_action( 'wp_enqueue_scripts', 'postValidator_customcss');
+
+
+/* INCLUDE FILES */
+include $plugin_url . '/includes/advancedCustomFields.php';
+
+?>
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Funktionaliät
+
+## Überprüfung Kontingent
+## Überprüfung Ablaufdatum
+
+
+# Plugin does not Provid Output
+This plugin does not provide the output of the sponsoring data. This must be done via template in theme.
+
